6e2467697dac46ac1dc0eaa76463c21d0f948855
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-27 15:19:32 -0500

    Add react-assignment-1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
                 { name: event.target.value, age: 21 },
                 { name: 'Neto', age: 20 },
                 { name: 'Brana', age: 20 },
-                { name: 'Counter', age: 20 } 
+                { name: 'Counter', age: 20 }
             ]
         })
     }
@@ -41,37 +41,46 @@ class App extends Component {
     // this.switchNameHandler.bin(this, 'Daniel') is the preferred way.
     // DON'T DO THIS () => this.switchNameHandler('Danny')
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
-            <div className="App">
+            <div className="App" >
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-                <button onClick={() => this.switchNameHandler('Danny')}>Switch name</button>
-                <Person 
-                    name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age} 
+            </p>
+                <button onClick={this.switchNameHandler.bind(this, 'Danny')}
+                style={style}>Switch name</button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
                     click={this.switchNameHandler.bind(this, 'Danny')}
                     changed={this.nameChangedHandler}>My hobbies: Programming and Anime</Person>
-                <Person 
-                    name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age} 
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
                     click={this.switchNameHandler.bind(this, 'NetOS')}>My hobbies: Programming and Wrestling</Person>
-                <Person 
-                    name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age} 
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
                     click={this.switchNameHandler.bind(this, 'Diego')}>My hobbies: Programming and going to the Gym</Person>
-                <Person 
-                    name={this.state.persons[3].name} 
-                    age={this.state.persons[3].age} 
+                <Person
+                    name={this.state.persons[3].name}
+                    age={this.state.persons[3].age}
                     click={this.switchNameHandler.bind(this, 'Counter')}>My hobbies: Programming and playing video games</Person>
             </div>
         );
         // return React.createElement('div', null, React.createElement('h1', null, 'Hello!'));
         //}
-    }
+    };
 }
 export default App;
