92f894fb3247b48cd2f5082a593ee95ad5ccdfe4
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-09-06 17:05:37 -0500

    Modify react-complete-guide

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-//import './Person.css';
+import classes from './Person.css';
 
+/*
 const StyledDiv = styled.div `width: 60%;
 margin: auto;
 border: 1px solid #eee;
@@ -12,14 +13,17 @@ text-align: center;
 @media(min-width: 500px) {
     width: 450px;
 }`
+*/
 
 const person = (props) => {
     return (
-        <StyledDiv>
+        //<StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>Hello! My name is {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
-        </StyledDiv>
+        </div>
+        //</StyledDiv>
     )
 };
 
